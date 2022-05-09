4fba0cebd1f1892afbb5bd9dfb68b92695cd4c32
jarray-intissar <intissarjarray.50@gmail.com>
2022-05-10 00:23:18 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Style.css';
+import mypht from "./photo.jpg";
+
+
+
+function App() {
+  return (
+    <div className="App">     
+<div style={{border:'solid 1px black' , maxWidth:'100vw'}}>
+
+<h1 className="title red">I N T I S S A R</h1>
+
+<br />
+
+<img src={mypht} className='photo' alt ='myphoto'/>
+
+<br />
+
+<img src="image.jpg" className='image' alt = 'myimage' />
+
+</div>
+
+<video className="video" width ="320"  height ="240"  controls >
+
+<source src="myvideo.mp4" type="video/mp4" />
+
+</video>
+    </div>
+  );
+}
+
+export default App;
+
